fca6ebbe7ccca8014f5734068b466b7b3077d216
Shahab Movahhedi <45717968+movahhedi@users.noreply.github.com>
2025-08-26 17:14:32 +0330

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
 	root: "./src",
 	build: {
 		rollupOptions: {
-			input: ["./src/index.html", "./src/new-tab.html"],
+			input: ["./src/new-tab.html"],
 		},
 		outDir: "../dist",
 	},
